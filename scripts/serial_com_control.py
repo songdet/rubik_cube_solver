f51fa17d8e23b7b93ac21a2af9dba3a8e0c61729
Songdet Nillasithanukroh <songdet@gmail.com>
2022-04-06 10:41:32 -0400

    modify script for two modes

--- scripts/serial_com_control.py
+++ scripts/serial_com_control.py
@@ -2,8 +2,8 @@ from enum import Enum
 import serial
 
 class Button(Enum):
-    A = 0,
-    B = 1
+    GRIPPER = 0,
+    ARM = 1
 
 # Print welcome message
 print("====================================================================================")
@@ -17,28 +17,46 @@ while True:
 
     # Block read until data is received
     val = ser.readline()
-    selected_button = Button.A
+    selected_button = Button.GRIPPER
     if val == b"A\n":
-        print("Button A pressed. Top and bottom grippers are now selected.")
-        selected_button = Button.A
+        print("Button A pressed. Gripper controls now selected.")
+        selected_button = Button.GRIPPER
     elif val == b"B\n":
-        print("Button B pressed. Left and right grippers are now selected.")
-        selected_button = Button.B
+        print("Button B pressed. Arm controls now selected")
+        selected_button = Button.ARM
 
-    # Prompt for the speed with which to turn the servo motor
-    speed = input("Select a speed for the motor between 1 (slowest) to 60 (highest): ")
-    while not speed.isdigit() or int(speed) < 1 or int(speed) > 60:
-        speed = input("Invalid input. Try selecting a value between 1-60: ")
-    write_byte = int(speed).to_bytes(1, 'little')
+    if selected_button == Button.GRIPPER:
 
-    # Send back command to test receiver
-    ser.write(write_byte)
+        # Prompt for the speed with which to turn the servo motor
+        speed = input("Select a speed for the motor between 1 (slowest) to 60 (highest): ")
+        while not speed.isdigit() or int(speed) < 1 or int(speed) > 60:
+            speed = input("Invalid input. Try selecting a value between 1-60: ")
+        speed = int(speed)
+        write_byte = speed.to_bytes(1, 'little')
 
-    # Print out confirmation that command was sent
-    if selected_button == Button.A:
-        print("Top and bottom grippers should now be turning with speed %s" % speed)
+        # Send back command to test receiver
+        ser.write(write_byte)
+
+        # Print output to let us know operation completed successfully
+        print("Grippers should now be turning with speed %s" % speed)
+
     else:
-        print("Left and right grippers should now be turning with speed %s" % speed)
+
+        # Prompt for direction of rotation
+        direction = input("Enter 1 to extend the arm and 2 to retract the arm: ")
+        while not direction.isdigit() or ((not int(direction) == 1) and (not int(direction) == 2)):
+            direction = input("Invalid input. Select 1 to extend and 2 to retract the arm: ")
+        direction = int(direction)
+        write_byte = direction.to_bytes(1, 'little')
+        
+        # Send back command to receiver
+        ser.write(write_byte)
+
+        # Print output to let us know operation completed successfully
+        if direction == 1:
+            print("Arm should now be extending.")
+        else:
+            print("Arm should now be retracting.")
 
     # print wait for next command
     print("\n====================================================================================")
